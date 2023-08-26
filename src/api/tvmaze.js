0cde673d785ac2180648e86a61eb10316809ce87
Aratrik <aratrikmukherjee2000@gmail.com>
2023-08-26 19:42:35 +0530

    fetch data inside show component

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -6,3 +6,4 @@ const apiGet = async queryString => {
 };
 export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
+export const getShowById = showId => apiGet(`/shows/${showId}`);
