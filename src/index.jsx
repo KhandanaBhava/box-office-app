e1d3215c35c2155ce6f5649113f2e5797b2d17ae
Aratrik <aratrikmukherjee2000@gmail.com>
2023-08-27 10:42:47 +0530

    installed react-query and used useQuery hook

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,11 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
