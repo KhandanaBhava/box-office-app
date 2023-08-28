5b476c526a124c29ba1e306d84745d4612072e0f
Aratrik <aratrikmukherjee2000@gmail.com>
2023-08-28 17:17:04 +0530

    fetched starred shows data

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://api.tvmaze.com';
+
 const apiGet = async queryString => {
   const response = await fetch(`${BASE_URL}${queryString}`);
   const body = await response.json();
@@ -8,3 +9,8 @@ export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
 export const getShowById = showId =>
   apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowsByIds = async showIds => {
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+  return Promise.all(promises);
+};
