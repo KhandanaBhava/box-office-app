f898329fd41421c58a1e76f55b06dc9ec8aaa1ec
Aratrik <aratrikmukherjee2000@gmail.com>
2023-08-29 17:12:06 +0530

    movie to show

--- src/components/AppTitle.jsx
+++ src/components/AppTitle.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export default function AppTitle(props) {
   const {
     title = 'Box Office',
-    subtitle = 'Are you lookig for a movie or an actor?',
+    subtitle = 'Are you lookig for a show or an actor?',
   } = props;
   return (
     <TitleWrapper>
